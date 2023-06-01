57432b45ad5d3e3cb761ac9143450863d8f0ad51
matticala <mm.calabro@outlook.com>
2023-06-01 15:50:01 +0200

    simplified Fish configuration to speed-up init

--- config.fish
+++ config.fish
@@ -1,27 +1,19 @@
 set fish_greeting
+eval (/opt/homebrew/bin/brew shellenv)
 
-# PATH first
-set -l brew_openssl_path /usr/local/opt/openssl@1.1
-test -e $brew_openssl_path && fish_add_path $brew_openssl_path/bin
+set -l cargo_home {$HOME}/.cargo
+set -l brew_libressl_path {$HOMEBREW_PREFIX}/opt/libressl
 
-set -l brew_node_path /usr/local/opt/node@14 || true
-test -e $brew_node_path && fish_add_path $brew_node_path/bin || true
-
-## Java
-set -l brew_java_path /usr/local/opt/java || true
-if test -e $brew_java_path
-  fish_add_path $brew_java_path/bin
-  set -gx CPPFLAGS "-I$brew_java_path/include"
-end
-
-type -q cargo && fish_add_path {$HOME}/.cargo/bin || true
-type -q go && fish_add_path (go env GOPATH)/bin || true
-
-## Then completions
-type -q direnv && direnv hook fish | source || true
-
+set -gx EDITOR "zed --wait"
+set -gx LDFLAGS "-L$brew_libressl_path/lib"
+set -gx CPPFLAGS "-I$brew_libressl_path/include"
+set -gx GOPATH {$HOME}/.go
 set -gx GPG_TTY (tty)
 
-## iTerm2
-# set -l iterm2_shell_integration {$HOME}/.iterm2_shell_integration.fish
-# test -e $iterm2_shell_integration && source $iterm2_shell_integration || true
+fish_add_path $GOPATH/bin $cargo_home/bin $brew_libressl_path/bin
+
+if status is-interactive
+    starship init fish | source
+    direnv hook fish | source
+    zoxide init --cmd cd fish | source
+end
