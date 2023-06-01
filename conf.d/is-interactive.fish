d7720c5095c3ba1759d254bd70a12db8836ceafb
matticala <mm.calabro@outlook.com>
2023-06-01 15:54:33 +0200

    moved interactive shell init to a dedicated file

--- conf.d/is-interactive.fish
+++ conf.d/is-interactive.fish
@@ -5,4 +5,8 @@ if status is-interactive
     alias find="fd"
     alias ps="procs"
     alias du="dust"
+
+    starship init fish | source
+    direnv hook fish | source
+    zoxide init --cmd cd fish | source
 end
--- config.fish
+++ config.fish
@@ -8,12 +8,7 @@ set -gx EDITOR "zed --wait"
 set -gx LDFLAGS "-L$brew_libressl_path/lib"
 set -gx CPPFLAGS "-I$brew_libressl_path/include"
 set -gx GOPATH {$HOME}/.go
-set -gx GPG_TTY (tty)
 
 fish_add_path $GOPATH/bin $cargo_home/bin $brew_libressl_path/bin
 
-if status is-interactive
-    starship init fish | source
-    direnv hook fish | source
-    zoxide init --cmd cd fish | source
-end
+set -gx GPG_TTY (tty)
